86f616ed20dd354a6d147e68201c43f8aef81892
rpw <109024183+pk1302@users.noreply.github.com>
2026-01-04 08:21:05 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // Assumes App.tsx is in the same folder
-import './index.css' // Optional if you have CSS
+import App from './App'
+import './index.css'  // <--- THIS LINE IS CRITICAL
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
